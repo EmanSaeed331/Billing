242dd7401f7bbb262708bfbd1699b9cc0f12892d
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-08-28 06:10:04 +0200

    add: product service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const users = require('./users/users.service.js');
+const products = require('./products/products.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
+  app.configure(products);
 };
